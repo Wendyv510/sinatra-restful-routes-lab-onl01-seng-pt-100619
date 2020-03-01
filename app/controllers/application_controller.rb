e6d13961a7be8b31df7bb62e90ce2c04a3554b51
Wendy Harris <wendyv510@hotmail.com>
2020-03-01 23:18:30 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes/new' do 
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time]) 
     @recipe.save
-    redirect to "/recipes/#{@recipe.id"
-  end 
+    redirect to "/recipes/#{@recipe.id}"
   
   get '/recipes/:id' do 
     @recipe = Recipe.find_by_id(params[:id]) 
